3b84b1f1fa4cd6e980ed394f84d68a59460c04d7
saisrivenkat <50038114+saisrivenkat@users.noreply.github.com>
2021-04-22 11:38:23 +0530

    showing total cart value

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Products from './Components/Products'
 import Cart from './Components/Cart'
 function App() {
@@ -11,7 +11,7 @@ function App() {
     {
       id: 2,
       title: "iphone",
-      cost: 120000,
+      cost: 100,
     },
     {
       id: 3,
@@ -21,21 +21,41 @@ function App() {
   ])
   const [cart, setcart] = useState([])
 
+  useEffect(() => {
+    cartcost()
+  }, [cart])
+
   const addcart = (id) => {
     const productid = products.findIndex(ul => ul.id === id)
-    const productinfo = { ...products[productid] }
+    const productinfo = { ...products[productid],qty:1 }
     setcart([...cart, productinfo])
 
   }
   const deleteitem = (id) => {
     setcart(cart.filter(ul => ul.id !== id))
   }
+  const cartcost = () => {
+    let cost = cart.reduce(function (prev, cur) {
+      return parseInt(prev + parseInt(cur.cost));
+    }, 0);
+    console.log(cost)
+  }
+  const qtychange =(e) =>{
+   {/*} const itemid = cart.findIndex(ul=>ul.id === id)
+    const item = {...cart[itemid]}
+    item.qty = e.target.value
+    const items =[...cart]
+    items[itemid]=item
+  setcart(items)*/}
+  console.log(e)
+    
 
+  }
   return (
     <div className="App">
       <h1>E-Commerce</h1>
       <Products products={products} addcart={(id) => addcart(id)} />
-      <Cart cart={cart} deleteitem={deleteitem} />
+      <Cart cart={cart} deleteitem={deleteitem} change={()=>qtychange()} cost={cartcost} />
 
     </div>
   );
--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,14 +1,18 @@
 import React from 'react'
 
-export default function Cart({ cart, deleteitem }) {
-   
+export default function Cart({ cart, deleteitem, change }) {
+
     return (
         <>
             {cart.map((item, index) => {
                 return (
                     <div>
                         <span>{item.title}</span>
-                        <button onClick={()=>deleteitem(item.id)}>Delete</button>
+                        <button onClick={() => deleteitem(item.id)}>Delete</button>
+                        <select onChange={change}>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                        </select>
                     </div>
 
                 )
